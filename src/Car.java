177cdcdf38c1a3e177aa8d6fc3ee916867c8d85a
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-11-03 20:14:25 +0300

    ООП.Введение.Домашнее задание 2.Задание 2.

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,27 @@
 public class Car {
+    String brand;
+    String model;
+    float engineVolume;
+    String color;
+    int year;
+    String country;
+    Car(String brand,String model,float engineVolume,String color,int year,String country){
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        if (year >= 0) {
+            this.year = year;
+        } else {
+            this.year = Math.abs(year);
+        }
+
+        this.country = country;
+    }
+    public String toString() {
+        return "В наличии автомобиль " + brand + " " + model + "." + "Год выпуска: " + year + "." +
+                "Сборка " + country + "." + "Цвет " + color + "." + "Объём двигателя: " + engineVolume + " л.";
+    }
+
 
 }
